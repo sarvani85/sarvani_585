d4dc8e18f1b130a96972999cd860cdf806a3eda0
sarvani <sarvanikilli585@gmail.com>
2019-09-27 15:34:38 +0530

    my work log upto updated

---
+++ demo.py
@@ -0,0 +1,14 @@
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def mul(a,b):
+    print(a*b)
+def div(a,b):
+    print(a/b)
+def f_div(a,b):
+    print(a//b)
+def m_div(a,b):
+    print(a%b)
+    
+    
